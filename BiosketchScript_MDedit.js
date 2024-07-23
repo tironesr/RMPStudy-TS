ae18430fa70418fa2cd4b586b4f26faec7058789
mtdao12 <83317758+mtdao12@users.noreply.github.com>
2024-07-23 12:48:55 -0700

    Update BiosketchScript_MDedit.js

--- BiosketchScript_MDedit.js
+++ BiosketchScript_MDedit.js
@@ -1,7 +1,7 @@
 // 3-minute timer
 setTimeout(function(){
 document.getElementById('submit').style.display = 'inline';
-}, 30); // timer was originally 180000
+}, 300); // timer was originally 180000
 
 const queryString = window.location.search;
 console.log(queryString);
