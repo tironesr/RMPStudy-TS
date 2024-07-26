a6167483bd01115bda98b4404aaca1d9442f702c
mtdao12 <83317758+mtdao12@users.noreply.github.com>
2024-07-25 19:28:59 -0700

    Update BiosketchScript_MDedit.js

--- BiosketchScript_MDedit.js
+++ BiosketchScript_MDedit.js
@@ -1,26 +1,15 @@
 // 3-minute timer
 setTimeout(function(){
 document.getElementById('submit').style.display = 'inline';
-}, 300); // timer was originally 180000
+}, 30); // timer was originally 180000
 
 const queryString = window.location.search;
 console.log(queryString);
 
 // save form data
 
+
 // get Participnat ID in URL
 var urlvar = jsPsych.data.getURLVariable();
 jsPsych.data.addProperties({participant_id: urlvar.PID});
 console.log(urlvar.PID);
-
-
-// POST data into OSF
-const filename = `${urlvar.PID}.csv`;
-
-const save_data = {
-type: jsPsychPipe,
-action: "save",
-experiment_id: "B2ZcxsecKMWu",
-filename: filename,
-data_string: ()=>jsPsych.data.get().csv()
-};
