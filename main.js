9d5c7cf151d393d68e807d9dec75f15a7698303f
mtdao12 <83317758+mtdao12@users.noreply.github.com>
2024-07-31 18:36:38 -0700

    Update and rename BiosketchScript_MDedit.js to main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// 3-minute timer
+setTimeout(function(){
+document.getElementById('submit').style.display = 'inline';
+}, 30); // timer was originally 180000
+
+const queryString = window.location.search;
+console.log(queryString);
+
+// experiment ID
+const experimentID = "B2ZcxsecKMWu"
