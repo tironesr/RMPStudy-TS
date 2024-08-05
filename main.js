bfba4761f9e4386d0d39f7476eeb1f3df71f4568
mtdao12 <83317758+mtdao12@users.noreply.github.com>
2024-08-04 19:21:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,26 @@ setTimeout(function(){
 document.getElementById('submit').style.display = 'inline';
 }, 30); // timer was originally 180000
 
+// get PID from URL parameters
 const queryString = window.location.search;
 console.log(queryString);
 
 // experiment ID
 const experimentID = "B2ZcxsecKMWu"
+
+// records start time when page loads
+let startTime;
+
+window.onload = function() {
+    startTime = new Date().getTime();
+};
+
+// records end time when page unloads
+window.onbeforeunload = function() {
+    const endTime = new Date().getTime();
+    const timeSpent = endTime - startTime; // Time in milliseconds
+    
+    console.log(`User spent ${timeSpent / 1000} seconds on the page.`);
+    
+    // Optionally send this data to a server or analytics service
+};
