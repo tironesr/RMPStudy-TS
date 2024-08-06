a89f3bc9293dd7a9f565b9efdbc79e929146bff1
tironesr <73726058+tironesr@users.noreply.github.com>
2024-08-05 23:03:11 -0700

    Update connecting.js
    Remove "unhide" after 25 sec

--- connecting.js
+++ connecting.js
@@ -1,8 +1,3 @@
  setTimeout(function(){
  document.getElementById('spinner').classList.remove('hide');
  }, 250);
-
-setTimeout(function(){
- document.getElementById('footer').classList.remove('hide');
- }, 20000);
- 
